2489d30756dc5a04551753a7d769d20702aa7787
Minhyuk Kim <labcs@naver.com>
2024-09-17 13:25:21 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/submit', (req, res, next) => {
+    const { name, password } = req.body
+    console.log(name, password)
+
+    res.send({
+        name,
+        password,
+        message: 'Success to post form data!!!'
+    })
+})
+
+app.use((req, res, next) => {
+    throw new Error('cannot find this route');
+})
+
+app.use((error, req, res, next) => {
+    res.send({
+        message: 'cannot find your fetch data...'
+    })
+})
+
+app.listen(5000);
